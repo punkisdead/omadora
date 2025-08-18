b1033bb856b2a087aa0dc7f78bbc21f5265179bc
Jeremy Anderson <jeremy.d.anderson@gmail.com>
2025-08-18 09:44:51 -0400

    update logos

--- boot.sh
+++ boot.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
-ansi_art='                 ▄▄▄                                                   
- ▄█████▄    ▄███████████▄    ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄ 
+ansi_art=' ▄█████▄   ▄███████████▄   ▄███████   ▄██████▄    ▄█████▄    ▄███████   ▄███████
 ███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   █▀   ███   ███  ███   ███
-███   ███  ███   ███   ███ ▄███▄▄▄███ ▄███▄▄▄██▀  ███       ▄███▄▄▄███▄ ███▄▄▄███
-███   ███  ███   ███   ███ ▀███▀▀▀███ ▀███▀▀▀▀    ███      ▀▀███▀▀▀███  ▀▀▀▀▀▀███
-███   ███  ███   ███   ███  ███   ███ ██████████  ███   █▄   ███   ███  ▄██   ███
+███   ███  ███   ███   ███  ███   ███  ███    ███ ███   ███  ███   ███  ███   ███
+███   ███  ███   ███   ███ ▄███▄▄▄███  ███    ███ ███   ███ ▄███▄▄▄██▀ ▄███▄▄▄███
+███   ███  ███   ███   ███ ▀███▀▀▀███  ███    ███ ███   ███ ▀███▀▀▀▀   ▀███▀▀▀███
+███   ███  ███   ███   ███  ███   ███  ███    ███ ███   ███ ██████████  ███   ███
 ███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
- ▀█████▀    ▀█   ███   █▀   ███   █▀   ███   ███  ███████▀   ███   █▀    ▀█████▀ 
-                                       ███   █▀                                  '
+ ▀█████▀   ▀█    ███   █▀   ███   █▀   ▀██████▀    ▀█████▀   ███   ███  ███   █▀'
 
 clear
 echo -e "\n$ansi_art\n"
 
-sudo pacman -Sy --noconfirm --needed git
+sudo dnf -y install git
 
-# Use custom repo if specified, otherwise default to basecamp/omarchy
-OMARCHY_REPO="${OMARCHY_REPO:-basecamp/omarchy}"
+# Use custom repo if specified, otherwise default to your-github-username/omadora
+OMADORA_REPO="${OMADORA_REPO:-your-github-username/omadora}"
 
-echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
-rm -rf ~/.local/share/omarchy/
-git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/null
+echo -e "\nCloning Omadora from: https://github.com/${OMADORA_REPO}.git"
+rm -rf ~/.local/share/omadora/
+git clone "https://github.com/${OMADORA_REPO}.git" ~/.local/share/omadora >/dev/null
 
 # Use custom branch if instructed
-if [[ -n "$OMARCHY_REF" ]]; then
-  echo -e "\eUsing branch: $OMARCHY_REF"
-  cd ~/.local/share/omarchy
-  git fetch origin "${OMARCHY_REF}" && git checkout "${OMARCHY_REF}"
+if [[ -n "$OMADORA_REF" ]]; then
+  echo -e "\eUsing branch: $OMADORA_REF"
+  cd ~/.local/share/omadora
+  git fetch origin "${OMADORA_REF}" && git checkout "${OMADORA_REF}"
   cd -
 fi
 
 echo -e "\nInstallation starting..."
-source ~/.local/share/omarchy/install.sh
+source ~/.local/share/omadora/install.sh
+
