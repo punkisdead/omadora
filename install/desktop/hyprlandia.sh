80a2c8b410d25f89eb6492561df51ebc14dc4413
Jeremy Anderson <jeremy.d.anderson@gmail.com>
2025-08-19 10:30:01 -0400

    fix(install): Remove swayosd and duplicate swaybg

--- install/desktop/hyprlandia.sh
+++ install/desktop/hyprlandia.sh
@@ -6,5 +6,5 @@ sudo dnf -y copr enable solopasha/hyprland
 # Install Hyprland and its ecosystem
 sudo dnf -y install \
   hyprland hyprshot hyprpicker hyprlock hypridle hyprsunset polkit-gnome \
-  walker libqalculate waybar mako swaybg swayosd \
-  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+  walker libqalculate waybar mako swaybg 
+xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
