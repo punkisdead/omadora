f5d60eb61cab136c58fb6174f5b6645bc2561ae8
Jeremy Anderson <jeremy.d.anderson@gmail.com>
2025-08-19 09:27:32 -0400

    fix(install): Reformat terminal.sh to fix execution error

--- install/development/terminal.sh
+++ install/development/terminal.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 sudo dnf -y install \
-  wget curl unzip 
-  fd-find fzf ripgrep zoxide bat jq xmlstarlet 
-  wl-clipboard fastfetch btop 
-  man-db tldr less whois plocate bash-completion 
+  wget curl unzip \
+  fd-find fzf ripgrep zoxide bat jq xmlstarlet \
+  wl-clipboard fastfetch btop \
+  man-db tldr less whois plocate bash-completion \
   alacritty
-
