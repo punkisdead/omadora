31c9cc1b9728d035ae5ca1beeae14abd97bfa68d
Jeremy Anderson <jeremy.d.anderson@gmail.com>
2025-08-19 09:23:35 -0400

    fix(install): Remove eza and inetutils from terminal tools

--- install/development/terminal.sh
+++ install/development/terminal.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sudo dnf -y install \
-  wget curl unzip inetutils \
-  fd-find eza fzf ripgrep zoxide bat jq xmlstarlet \
-  wl-clipboard fastfetch btop \
-  man-db tldr less whois plocate bash-completion \
+  wget curl unzip 
+  fd-find fzf ripgrep zoxide bat jq xmlstarlet 
+  wl-clipboard fastfetch btop 
+  man-db tldr less whois plocate bash-completion 
   alacritty
+
