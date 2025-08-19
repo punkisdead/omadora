6abd1cd2937415c3ae6d853a052c407d57d5035c
Jeremy Anderson <jeremy.d.anderson@gmail.com>
2025-08-19 10:41:23 -0400

    feat(install): Replace walker with wofi

--- install/desktop/hyprlandia.sh
+++ install/desktop/hyprlandia.sh
@@ -6,5 +6,5 @@ sudo dnf -y copr enable solopasha/hyprland
 # Install Hyprland and its ecosystem
 sudo dnf -y install \
   hyprland hyprshot hyprpicker hyprlock hypridle hyprsunset mate-polkit \
-  walker libqalculate waybar mako swaybg \
+  wofi waybar mako swaybg \
   xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
