e8c6d3b268dc9fd47a3e47a76d60bb44ebe3e2b5
Jeremy Anderson <jeremy.d.anderson@gmail.com>
2025-08-19 10:36:02 -0400

    fix(install): Use mate-polkit and refresh repos

--- install/desktop/hyprlandia.sh
+++ install/desktop/hyprlandia.sh
@@ -2,9 +2,10 @@
 
 # Enable the COPR repository for Hyprland and related tools
 sudo dnf -y copr enable solopasha/hyprland
+sudo dnf -y check-update
 
 # Install Hyprland and its ecosystem
 sudo dnf -y install \
-  hyprland hyprshot hyprpicker hyprlock hypridle hyprsunset polkit-gnome \
-  walker libqalculate waybar mako swaybg 
-xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+  hyprland hyprshot hyprpicker hyprlock hypridle hyprsunset mate-polkit \
+  walker libqalculate waybar mako swaybg \
+  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
