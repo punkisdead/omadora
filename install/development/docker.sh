f757f6d6da8620bbde6088b6bdca4a3ec8b47723
Jeremy Anderson <jeremy.d.anderson@gmail.com>
2025-08-19 10:01:49 -0400

    fix(install): Use wget to add docker repo

--- install/development/docker.sh
+++ install/development/docker.sh
@@ -2,7 +2,7 @@
 
 # Add the official Docker repository
 sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo wget https://download.docker.com/linux/fedora/docker-ce.repo -O /etc/yum.repos.d/docker-ce.repo
 
 # Install Docker Engine
 sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
